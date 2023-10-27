97250006add7b942c447a426550385668e9f6da1
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2023-10-27 21:27:18 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,67 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import TableComponent from './TableComponent'
-import AddTransaction from './assets/AddTransaction'
+import  { useState, useEffect } from 'react';
+import './App.css';
+import TableComponent from './TableComponent';
+import AddTransaction from './assets/AddTransaction';
 
 function App() {
-  const [transactions, setTransactions]= useState([])
+  const [transactions, setTransactions] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://localhost:3000/transactions")
-    .then(res =>res.json())
-    .then(transaction=>setTransactions(transaction))
-     .catch((e) => console.log(e))
-  
-  } ,[])
- 
-//console.log(transactions)
+      .then((res) => res.json())
+      .then((transaction) => setTransactions(transaction))
+      .catch((e) => console.log(e));
+  }, []);
+
+  // Function to add a new transaction
+  function addTransaction(newTransaction) {
+    // Make a POST request to your server to add the transaction.
+    fetch("http://localhost:3000/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTransaction),
+    })
+      .then(async (response) => {
+        if (response.status === 201) {
+          // If the request was successful, update the client state.
+          setTransactions([...transactions, newTransaction]);
+        } else {
+          console.error("Error posting transaction");
+        }
+      });
+  }
+
+  // Function to delete a transaction by ID
+  function deleteTransaction(transactionId) {
+    // Make a DELETE request to your server to delete the transaction.
+    fetch(`http://localhost:3000/transactions/${transactionId}`, {
+      method: "DELETE",
+    })
+      .then(async (response) => {
+        if (response.status === 201) {
+          // If the request was successful, update the client state.
+          const updatedTransactions = transactions.filter(
+            (transaction) => transaction.id !== transactionId
+          );
+          setTransactions(updatedTransactions);
+        } else {
+          console.error("Error deleting transaction");
+        }
+      });
+  }
 
   return (
     <>
-    <h1>BANK OF FLATIRON</h1>
-<TableComponent transactions={transactions} />
-<AddTransaction/>
-
-
-
-
-
-
-
-
-
-
-{/*    
-      {transactions.map((transaction) =>(
-        <div key = {transaction.id}>
-        <p> {transaction.id}</p>
-        <p> {transaction.date}</p>
-        <p> {transaction.description}</p>
-        <p> {transaction.category}</p>
-        <p> {transaction.amount}</p>
-        </div>
-      ))}
-      */}
+      <h1>BANK OF FLATIRON</h1>
+      <TableComponent
+        transactions={transactions}
+        onDeleteTransaction={deleteTransaction}
+      />
+      <AddTransaction onAddTransaction={addTransaction} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
