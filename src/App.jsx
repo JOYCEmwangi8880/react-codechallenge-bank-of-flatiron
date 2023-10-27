21097c944ceffe5aeb76b693f4d4a996a9eae48c
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2023-10-27 13:36:08 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import TableComponent from './TableComponent'
 
 function App() {
+  const [transactions, setTransactions]= useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:3000/transactions")
+    .then(res =>res.json())
+    .then(transaction=>setTransactions(transaction))
+     .catch((e) => console.log(e))
+  
+  } ,[])
  
+//console.log(transactions)
+
   return (
     <>
-      <div>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<TableComponent transactions={transactions} />
+
+
+
+
+
+
+
+
+
+{/*    
+      {transactions.map((transaction) =>(
+        <div key = {transaction.id}>
+        <p> {transaction.id}</p>
+        <p> {transaction.date}</p>
+        <p> {transaction.description}</p>
+        <p> {transaction.category}</p>
+        <p> {transaction.amount}</p>
+        </div>
+      ))}
+      */}
     </>
   )
 }
